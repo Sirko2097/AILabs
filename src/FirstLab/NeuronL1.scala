464bd5d7ca259bdef56cc69d46ef6be15b2c64e7
Serhii <sirko2097@gmail.com>
2018-09-12 09:40:26 +0300

    Added new method and explanation.

--- src/FirstLab/NeuronL1.scala
+++ src/FirstLab/NeuronL1.scala
@@ -6,22 +6,41 @@ class NeuronL1(val x: Double, val yExpected: Double, val weight: Double, val del
 
   var w: Double = weight
 
+
+  /**
+    * In this method neuron trains.
+    * @return velue of weight
+    * */
   def training() = {
     var yi = 0.0
     var xi = 0.0
     do {
       xi = x * w
       yi = sigmoid(xi)
-      val di = yi * (1 - yi) * (yExpected - yi)
-      val dw = di * x
-      w = w + dw
+      val deltaI = sigmoidPrime(yi)
+      val deltaW = deltaI * x
+      w = w + deltaW
     } while (abs((yExpected - yi) / yExpected) > deltaMax)
     println("Res y: " + yi)
     w
   }
 
+  /**
+    * Calculate sigmoid
+    *
+    * @return value of function in x
+    * */
   private def sigmoid(x: Double) = {
     1 / (1 + exp(-x))
+  }
+
+  /**
+    * Calculate another version of sigmoid for changing weight
+    *
+    * @return value of function
+    * */
+  private def sigmoidPrime(y: Double) = {
+    y * (1 - y) * (yExpected - y)
   }
 
 }
